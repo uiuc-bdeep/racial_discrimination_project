ab8ca9c5920aa98a1f1bd418823ba600dd88db05
chkim122 <chkim12@illinois.edu>
2018-05-15 22:31:51 -0500

    updated code to handle 3 racial groups

--- scripts/trulia_rental_message_sender.py
+++ scripts/trulia_rental_message_sender.py
@@ -27,7 +27,11 @@ from selenium.common.exceptions import ElementNotVisibleException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+# COMMAND TO RUN THIS SCRIPT: python trulia_rental_message_sender.py trulia_rental_message_sender_data.txt <THE DAY YOU ARE RUNNING THIS SCRIPT[1-3]>
+
 custom_message = "I'm looking for a place in this neighborhood."
+
+number_of_identities_per_race = 6
 
 page_one_name__css = "#nameInput"  
 page_one_email_css = "#emailInput"
@@ -36,36 +40,29 @@ page_one_send__css = '#topPanelLeadForm > div > div > span > div > button'
 page_one_mess__css = '#topPanelLeadForm > div > div > span > div > div.madlibsForm.form > div:nth-child(6) > a'
 page_one_textb_css = '#textarea'
 
-def initialize_data_sheets(status_sheet,phone_status_sheet,df_input):
-	df_status_list = ['handled'     , 'timestamp 1'       , 'timestamp 2'          , 'timestamp 3'         , 'timestamp 4'             ,
-					  'timestamp 5' , 'timestamp 6'       , 'timestamp 7'          , 'timestamp 8'         , 'timestamp 9'             ,
-					  'timestamp 10', 'timestamp 11'      , 'timestamp 12'         ,
-					  'address 1'   , 'total responses 1' , 'response timestamp 1' , 'total unavailable 1' , 'unavailable timestamp 1' ,
-					  'address 2'   , 'total responses 2' , 'response timestamp 2' , 'total unavailable 2' , 'unavailable timestamp 2' ,
-					  'address 3'   , 'total responses 3' , 'response timestamp 3' , 'total unavailable 3' , 'unavailable timestamp 3' ,
-					  'address 4'   , 'total responses 4' , 'response timestamp 4' , 'total unavailable 4' , 'unavailable timestamp 4' ,
-					  'address 5'   , 'total responses 5' , 'response timestamp 5' , 'total unavailable 5' , 'unavailable timestamp 5' ,
-					  'address 6'   , 'total responses 6' , 'response timestamp 6' , 'total unavailable 6' , 'unavailable timestamp 6' , 
-					  'address 7'   , 'total responses 7' , 'response timestamp 7' , 'total unavailable 7' , 'unavailable timestamp 7' ,
-					  'address 8'   , 'total responses 8' , 'response timestamp 8' , 'total unavailable 8' , 'unavailable timestamp 8' ,
-					  'address 9'   , 'total responses 9' , 'response timestamp 9' , 'total unavailable 9' , 'unavailable timestamp 9' ,
-					  'address 10'  , 'total responses 10', 'response timestamp 10', 'total unavailable 10', 'unavailable timestamp 10',
-					  'address 11'  , 'total responses 11', 'response timestamp 11', 'total unavailable 11', 'unavailable timestamp 11',
-					  'address 12'  , 'total responses 12', 'response timestamp 12', 'total unavailable 12', 'unavailable timestamp 12']
+def initialize_data_sheets(status_sheet,phone_status_sheet,df_input,LPI):
+	df_status_list = ['handled']
+	for i in range(1,LPI + 1):
+		df_status_list.append('timestamp ' + str(i))
+	for i in range(1,LPI + 1): 
+		df_status_list.append('address ' + str(i))
+		df_status_list.append('total responses ' + str(i))
+		df_status_list.append('response timestamp 1' + str(i))
+		df_status_list.append('total unavailable ' + str(i))
+		df_status_list.append('unavailable ' + str(i))
+
 	# new column names for the phone sheet
-	df_phone_list = ['address 1' ,'voicemail responses 1' , 'voicemail timestamp 1' , 'voicemail minutes 1' , 'text responses 1' , 'text timestamp 1' , 'unavailable responses 1' , 'unavailable timestamp 1' ,
-					 'address 2' ,'voicemail responses 2' , 'voicemail timestamp 2' , 'voicemail minutes 2' , 'text responses 2' , 'text timestamp 2' , 'unavailable responses 2' , 'unavailable timestamp 2' ,
-					 'address 3' ,'voicemail responses 3' , 'voicemail timestamp 3' , 'voicemail minutes 3' , 'text responses 3' , 'text timestamp 3' , 'unavailable responses 3' , 'unavailable timestamp 3' ,
-					 'address 4' ,'voicemail responses 4' , 'voicemail timestamp 4' , 'voicemail minutes 4' , 'text responses 4' , 'text timestamp 4' , 'unavailable responses 4' , 'unavailable timestamp 4' ,
-					 'address 5' ,'voicemail responses 5' , 'voicemail timestamp 5' , 'voicemail minutes 5' , 'text responses 5' , 'text timestamp 5' , 'unavailable responses 5' , 'unavailable timestamp 5' ,
-					 'address 6' ,'voicemail responses 6' , 'voicemail timestamp 6' , 'voicemail minutes 6' , 'text responses 6' , 'text timestamp 6' , 'unavailable responses 6' , 'unavailable timestamp 6' ,
-					 'address 7' ,'voicemail responses 7' , 'voicemail timestamp 7' , 'voicemail minutes 7' , 'text responses 7' , 'text timestamp 7' , 'unavailable responses 7' , 'unavailable timestamp 7' ,
-					 'address 8' ,'voicemail responses 8' , 'voicemail timestamp 8' , 'voicemail minutes 8' , 'text responses 8' , 'text timestamp 8' , 'unavailable responses 8' , 'unavailable timestamp 8' ,
-					 'address 9' ,'voicemail responses 9' , 'voicemail timestamp 9' , 'voicemail minutes 9' , 'text responses 9' , 'text timestamp 9' , 'unavailable responses 9' , 'unavailable timestamp 9' ,
-					 'address 10','voicemail responses 10', 'voicemail timestamp 10', 'voicemail minutes 10', 'text responses 10', 'text timestamp 10', 'unavailable responses 10', 'unavailable timestamp 10',
-					 'address 11','voicemail responses 11', 'voicemail timestamp 11', 'voicemail minutes 11', 'text responses 11', 'text timestamp 11', 'unavailable responses 11', 'unavailable timestamp 11',
-					 'address 12','voicemail responses 12', 'voicemail timestamp 12', 'voicemail minutes 12', 'text responses 12', 'text timestamp 12', 'unavailable responses 12', 'unavailable timestamp 12',
-					 'undeterminable']
+	df_phone_list = []
+	for i in range(1,LPI+1):
+		df_phone_list.append('address ' + str(i)) 
+		df_phone_list.append('voicemail responses ' + str(i)) 
+		df_phone_list.append('voicemail timestamp ' + str(i)) 
+		df_phone_list.append('voicemail minutes ' + str(i)) 
+		df_phone_list.append('text responses ' + str(i)) 
+		df_phone_list.append('text timestamp ' + str(i)) 
+		df_phone_list.append('unavailable responses ' + str(i)) 
+		df_phone_list.append('unavailable timestamp ' + str(i)) 
+	df_phone_list.append('indeterminable')
 
 	# empty dataframe with new columns for status sheet 
 	df_new                = (pd.DataFrame(columns=df_status_list)).fillna(' ')
@@ -88,23 +85,47 @@ def initialize_data_sheets(status_sheet,phone_status_sheet,df_input):
 
 	return df_status, df_status_phone
 
-def get_dataframes(address_sheet, status_sheet, phone_status_sheet, rentals_sheet): 
+def get_dataframes(names_sheet, status_sheet, phone_status_sheet, LPI): 
+	df_input   = pd.read_csv(names_sheet) # create a dataframe with the return_sheet.
 	if os.path.isfile(status_sheet) == False: # check if the status sheet exists
 			print('Status Sheet Not Initialized\n Initializing ...')
-			df_status, df_status_phone = initialize_data_sheets(status_sheet,phone_status_sheet,df_input)
+			df_status, df_status_phone = initialize_data_sheets(status_sheet,phone_status_sheet,df_input, LPI)
 
-		else: 
-			# create a dataframe from saved status sheet csv
-			df_status = pd.read_csv(status_sheet)
-			df_status_phone  = pd.read_csv(phone_status_sheet)
+	else: 
+		# create a dataframe from saved status sheet csv
+		df_status = pd.read_csv(status_sheet)
+		df_status_phone  = pd.read_csv(phone_status_sheet)
 
-		df_input   = pd.read_csv(address_sheet) # create a dataframe with the return_sheet.
-		df_rentals = pd.read_csv(rentals_sheet) # create a dataframe with the rentals_sheet.
+	return df_input, df_status, df_status_phone
 
-	return df_input, df_rentals, df_status, df_status_phone
 def get_partitions(df_rentals): 
-	select_by = '3 BR'
+	# cut the main rentals dataframe down by a criteria MAY NEED CHANGING IN THE FUTURE
+	df_mod_rentals   = df_rentals[df_rentals['Bedroom_max'] == '3']
+	# equally divide up the dataframe based on the number of races, in this case we have white, black, and hispanic
+	split_df    = np.array_split(df_mod_rentals, 3)
+	group_one   = split_df[0]
+	group_two   = split_df[1]
+	group_three = split_df[2]
+	#  calculate the Listings Per Identity by getting the minimum of the three groups and dividing it by the number of identities per race
+	LPI         = min([len(group_one), len(group_two), len(group_three)])/number_of_identities_per_race
 
+	return group_one, group_two, group_three, LPI
+
+def get_day_dict(df_rentals, day_num):
+	 	group_one, group_two, group_three, LPI = get_partitions(df_rentals)
+		
+		day_trial_dict = {}
+		if day_num == 1:
+			day_trial_dict.update({'black': group_one, 'white': group_two, 'hispanic': group_three})
+		elif day_num == 2:
+			day_trial_dict.update({'black': group_two, 'white': group_thee, 'hispanic': group_one})
+		elif day_num == 3:  
+			day_trial_dict.update({'black': group_three, 'white': group_one, 'hispanic': group_two})
+		else: 
+			print('not a valid number')
+			exit()
+
+		return day_trial_dict, LPI
 
 def wait_and_get(browser, cond, maxtime): 
 	flag = True
@@ -140,15 +161,21 @@ def wait_and_get(browser, cond, maxtime):
 				browser.switch_to_window(browser.window_handles[0])
 				flag = True
 
-def get_prev_addr(handled_state): 
+def get_prev_addr(handled_state,race,df_status): 
 	# get a list of all of the previously inquired addresses
 	prev_addresses = []
+	print(race)
+	df_race = df_status[df_status['racial category'] == race]
+
 	if handled_state > 0: 
-		for i in range(1,handled_state + 1): 
-			prev_addresses.append((row["address " + str(i)][1:-2]).split(',')[0])
+		for index,row in df_race.iterrows():
+			for i in range(1,handled_state + 1): 
+				address_string = 'address ' + str(i)
+				if row[address_string] != None: 
+					prev_addresses.append((row[address_string][1:-2]).split(',')[0])
 	return prev_addresses
 
-def send_message(name, email, phone_num, url, custom_msg_bool):
+def send_message(name, email, phone_num, url):
 	fail_count = 0 # counter for how many times this script fails
 	while True: 
 		page_restart = 0 # tracker for restarting the finction
@@ -169,10 +196,9 @@ def send_message(name, email, phone_num, url, custom_msg_bool):
 			fail_count += 1
 			page_restart = 1
 			if fail_count > 3:
-				return "Failed: New Address"
+				return "NEED NEW ADDRESS"
 
 		if page_restart == 0: # check if the page needs to be restarted
-			page_num = 1 # indicates the page type CAN BE 1 OR 2 
 			send_handle = None # variable for the button click
 			try:
 				name_handle = driver.find_element_by_css_selector(page_one_name__css)
@@ -180,9 +206,8 @@ def send_message(name, email, phone_num, url, custom_msg_bool):
 				driver.quit()
 				display.stop()
 				print('Listing Already Sold')
-				return "Failed: New Address"
+				return "NEED NEW ADDRESS"
 
-			print('PAGE NUMBER IS ' + str(page_num))
 			# set the variables
 			name_css  = page_one_name__css
 			email_css = page_one_email_css
@@ -202,13 +227,13 @@ def send_message(name, email, phone_num, url, custom_msg_bool):
 			phone_handle = driver.find_element_by_css_selector(phone_css)
 			phone_handle.send_keys(str(phone_num))
 
-			if custom_msg_bool != True: 
-				message_handle = driver.find_element_by_css_selector(page_one_mess__css)
-				message_handle.click()
-				text_box       = driver.find_element_by_css_selector(page_one_textb_css)
-				text_box.send_keys(Keys.COMMAND + 'a')
-				text_box.send_keys(Keys.BACKSPACE) 
-				text_box.send_keys(custom_message)
+			# if custom_msg_bool != True: 
+			# 	message_handle = driver.find_element_by_css_selector(page_one_mess__css)
+			# 	message_handle.click()
+			# 	text_box       = driver.find_element_by_css_selector(page_one_textb_css)
+			# 	text_box.send_keys(Keys.COMMAND + 'a')
+			# 	text_box.send_keys(Keys.BACKSPACE) 
+			# 	text_box.send_keys(custom_message)
 
 
 			send_handle = driver.find_element_by_css_selector(send_css)
@@ -227,69 +252,79 @@ def send_message(name, email, phone_num, url, custom_msg_bool):
 
 			return time_sent
 
-parameter_file_name = sys.argv[1]
+parameter_file_name  = sys.argv[1]
+parameter_day_trial = sys.argv[2] # can be a value of 1,2,3. The will tell the script which partition to use for a race. 
 line_num = 1
 while True:
 	file_line = linecache.getline(parameter_file_name, line_num)
 	if file_line is '':
 		break
 	else:
-		file_line = re.sub('\n','',file_line)
-		parameters = file_line.split(",") # parse the text file by the \ character
+		while True: 
+			file_line = re.sub('\n','',file_line)
+			parameters = file_line.split(",") # parse the text file by the \ character
 
-		# set the string passed in from the text files
-		address_sheet 	   = parameters[0] # return_sheet
-		status_sheet  	   = parameters[1] # status sheet
-		phone_status_sheet = parameters[2] # phone output csv
-		rentals_sheet 	   = parameters[3] # csv that contains listings
+			# set the string passed in from the text files
+			names_sheet 	   = parameters[0] 			# name_market.csv
+			status_sheet  	   = parameters[1] 			# status sheet
+			phone_status_sheet = parameters[2] 			# phone output csv
+			rentals_sheet 	   = parameters[3] 			# csv that contains listings
 
-		# get all required dataframes 
-		df_input, df_rentals, df_status, df_status_phone = get_dataframes(address_sheet, status_sheet, phone_status_sheet, rentals_sheet)
-		
-		# check if all listing have been handled by checking the last value in the handled column
-		if df_status.iloc[-1]['handled'] == 12: 
-			print('All names have been handled\nexiting...')
-			exit()
+			df_rentals          = pd.read_csv(rentals_sheet)															# create a dataframe with the rentals_sheet.
+			df_rentals          = df_rentals[pd.notnull(df_rentals['Address'])].sample(frac = 1, random_state = 0)	 	# drop all the NA's in the original DF and shuffle
+			print(df_rentals)
+			day_trial_dict, LPI = get_day_dict(df_rentals, int(parameter_day_trial)) 									# get which partition of the df_rentals that a race will be sending inquiries to
 
-		rental_partitions = get_partitions(df_rentals)
-		# find the where the script left off last and set it to row_index
-		values = list(df_status['handled'].values)
-		row_index = values.index(min(values)) 
+			df_input, df_status, df_status_phone = get_dataframes(names_sheet, status_sheet, phone_status_sheet, LPI)	# get all required dataframes 
+			
+			if df_status.iloc[-1]['handled'] == LPI: 								# check if all listing have been handled by checking the last value in the handled column
+				print('All names have been handled\nexiting...')
+				exit()
 
-		# store the row of the dataframe where the script last left off
-		row = df_status.iloc[row_index]
-		handled_state = row['handled']
-		# store information about the person 
-		name = row['first name'] + ' ' + row['last name']
-		email = row['email']
-		phone_num = row['phone number']
+			values = list(df_status['handled'].values)								# find the where the script left off last and set it to row_index
+			row_index = values.index(min(values)) 
 
-		# randomly choose an address from rentals.csv
-		random_address = df_rentals.sample(n = 1)
-		address = random_address['Address'].values[0]
-		url = random_address['URL'].values[0]
+			# store the row of the dataframe where the script last left off
+			row              = df_status.iloc[row_index]
+			handled_state    = row['handled']
+			# store information about the person 
+			name             = row['first name'] + ' ' + row['last name']
+			email            = row['email']
+			phone_num        = row['phone number']
+			race             = row['racial category']
+			df_race_cur_addr = day_trial_dict[race]                           	 	# all of the listings that this race can choose from on this day
+			race_prev_addr   = get_prev_addr(handled_state, race, df_status)		# all of the listings that have been chosen on this day
+			df_race_cur_addr = df_race_cur_addr[~df_race_cur_addr['Address'].isin(race_prev_addr)]
 
-		address_string = str('address ' + str(handled_state+1))
-		time_stamp_col = str('timestamp ' + str(handled_state+1))
+			# create variables to store information about the listing that will be inquired
+			time_stamp = "NEED NEW ADDRESS"
+			address    = None
+			url        = None 
 
-		# send out the inquiry and return the timestamp of when the inquiry was sent out
-		time_stamp = send_message(name, email, phone_num, url)
+			# create strings that will be used to located where to store data in df
+			address_string  = str('address ' + str(handled_state+1))
+			time_stamp_col  = str('timestamp ' + str(handled_state+1))
 
- 		# in case the listing is taken off the market, try again 
- 		while time_stamp == "Failed: New Address":
- 			print('Acquiring New Address')
- 			random_address = df_rentals.sample(n = 1)
-			address = random_address['Address'].values[0]
-			url = random_address['URL'].values[0]
-			time_stamp = send_message(name, email, phone_num, url)
+	 		# keep trying new address until a valid address is found
+	 		while time_stamp == "NEED NEW ADDRESS":
+	 			print('Acquiring New Address')
+	 			random_address = df_race_cur_addr.sample(n = 1)					# randomly choose an address from rentals.csv
+				address        = random_address['Address'].values[0]
+				url            = random_address['URL'].values[0]
+				time_stamp     = send_message(name, email, phone_num, url)  	# send out the inquiry and return the timestamp of when the inquiry was sent out
+				if time_stamp == 'NEED NEW ADDRESS': 							# if the address is not able to be inquired then remove it from the list of possible listings to inquire
+					df_race_cur_addr = df_race_cur_addr[~df_race_cur_addr['Address'].isin([address])]
+				if df_race_cur_addr.empty: 										# if the list is empty, the skip
+					print('NO MORE ADDRESSES FOR ' + race + ' GROUP')
+					break
 
-		# increment the handled 
-		df_status.at[row_index,'handled'] += 1
+			# increment the handled 
+			df_status.at[row_index,'handled'] += 1
 
-		# update address/url and timstamp
-		df_status.set_value(row_index,address_string,str('(' + str(address) + ', '+ str(url) + ')'))
-		df_status.at[row_index,time_stamp_col] = time_stamp
+			# update address/url and timstamp
+			df_status.set_value(row_index,address_string,str('(' + str(address) + ', '+ str(url) + ')'))
+			df_status.at[row_index,time_stamp_col] = time_stamp
 
-		#write back to csv 
-		df_status.to_csv(status_sheet,mode='wb',index=False)
-		print('----------------------------------------------------------------------------')
+			#write back to csv 
+			df_status.to_csv(status_sheet,mode='wb',index=False)
+			print('----------------------------------------------------------------------------')
