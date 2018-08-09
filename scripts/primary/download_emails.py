b97bb1045928ae6c53af86463d839d2871dbdfe4
tphillips619 <thomasp3@illinois.edu>
2018-08-09 10:03:56 -0700

    Updated download_emails.py to pull from params.json

--- scripts/primary/download_emails.py
+++ scripts/primary/download_emails.py
@@ -5,24 +5,21 @@ import getpass
 import pandas as pd
 import json
 
+PROJ_ROOT = os.environ['PROJ_ROOT']
+with open(PROJ_ROOT + 'parameters.json') as parameter_json:
+    params = json.load(parameter_json)
+
 IMAP_SERVER = 'imap.gmail.com'
-EMAIL_FOLDERS=['GA-Response','GA-Nonresponse']
+EMAIL_FOLDERS= [params['EMAIL_PREFIX']+folder for folder in ['-Response','-Nonresponse']]
 EMAIL_DOMAIN ='@gmail.com'
 PASSWORD = 'BdeepTrulia'
+print(EMAIL_FOLDERS)
 
-PROJ_ROOT = os.environ['PROJ_ROOT']
+EMAIL_DIR = params['EMAIL_DIR']
+print(EMAIL_DIR)
 
-with open(PROJ_ROOT + 'parameters.json') as parameter_json:
-    parameters = json.load(parameter_json)
-
-PROJ_ROOT = parameters['PROJ_ROOT']
-TRIAL_DATA = parameters['TRIAL_DATA']
-NAMES_FILE = PROJ_ROOT +'project_files/names_market.csv'
-DATA_DIR = TRIAL_DATA+'emails/'
-
-names = pd.read_csv(NAMES_FILE)
-ACCOUNT_LIST = names['email'].values
-print (ACCOUNT_LIST)
+ACCOUNT_LIST = params['ACCOUNT_LIST']
+print(ACCOUNT_LIST)
 
 def process_mailbox(M, account_prefix, folder):
     """
@@ -47,37 +44,37 @@ def process_mailbox(M, account_prefix, folder):
         f.close()
 
 def main():
-    if not os.path.exists(DATA_DIR):
-        os.makedirs(DATA_DIR)
+    if not os.path.exists(EMAIL_DIR):
+        os.makedirs(EMAIL_DIR)
     for folder in EMAIL_FOLDERS:
-        if folder in os.listdir(DATA_DIR):
+        if folder in os.listdir(EMAIL_DIR):
             print("Found "+folder+" in data directory")
         else:
             print("Making folder " + folder)
-            os.mkdir(DATA_DIR+folder)
+            os.mkdir(EMAIL_DIR+folder)
 
-    data_home = DATA_DIR
-    os.chdir(data_home)
+    os.chdir(EMAIL_DIR)
     print(os.getcwd())
 
     for account in ACCOUNT_LIST:
         print(account)
         email_account = account + EMAIL_DOMAIN
 
-        M = imaplib.IMAP4_SSL(IMAP_SERVER)
-        M.login(email_account, PASSWORD)
+         M = imaplib.IMAP4_SSL(IMAP_SERVER)
+         M.login(email_account, PASSWORD)
 
         for folder in EMAIL_FOLDERS:
-            os.chdir(data_home+folder)
+            os.chdir(EMAIL_DIR+folder)
             print(os.getcwd())
             print("Processing mailbox: ", folder)
-            rv, data = M.select(folder)
-            if rv == 'OK':
-                process_mailbox(M,account,folder)
-                M.close()
-            else:
-                print ("ERROR: Unable to open mailbox ", rv)
-        M.logout()
+             rv, data = M.select(folder)
+             if rv == 'OK':
+                 process_mailbox(M,account,folder)
+                 M.close()
+             else:
+                 print ("ERROR: Unable to open mailbox ", rv)
+         M.logout()
 
 if __name__ == "__main__":
     main()
+
